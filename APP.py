2eb9d406ecfeb2c9e6e5bf449fc3904171877c89
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-19 13:48:36 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -7,4 +7,5 @@ st.title("Bienvenido a la InfVagAPP")
 
 sint=st.multiselect("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",("Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"))
 if sint== "Salida de líquido vaginal":
-    BalloonsMixin
+    Balloons
+st.title("Pueba")
