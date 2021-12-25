0ad970a25969fe62db03623d3aade33398541f35
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-25 11:39:12 -0700

    Update APP.py

--- APP.py
+++ APP.py
@@ -7,4 +7,7 @@ st.title("Bienvenido a la InfVagAPP")
 #APP para detectar infecciones vaginales y dar recomendaciones
 
 st.subheader("Esta es una aplicación de autocuestionamiento para orientación médica, no se registrará ningún dato que se ingresé y no se pedirá la identificación del usuario de ninguna manera (Nombre, Id, etc).")
-Sintomas=st.multiselect("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",["Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"])
+dolorvag="Dolor vaginal"
+Sintomas=st.multiselect("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",[dolorvag,"Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"])
+if Sintomas == dolorvag:
+    st.markdown("Duele")
