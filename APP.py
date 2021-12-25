9161248fe76202f973374a094e60b5fa073bd8e9
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-25 11:21:39 -0700

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from streamlit.elements.balloons import BalloonsMixin
 from streamlit.proto.Balloons_pb2 import Balloons
+from streamlit.proto.Button_pb2 import Button
 videomp=st.image("medpost.gif")
 st.title("Bienvenido a la InfVagAPP")
 #APP para detectar infecciones vaginales y dar recomendaciones
 
-sint=st.multiselect("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",("Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"))
-if sint== "Salida de líquido vaginal":
-    Balloons
-st.title("Pueba")
+st.subheader("Esta es una aplicación de autocuestionamiento para orientación médica, no se registrará ningún dato que se ingresé y no se pedirá la identificación del usuario de ninguna manera (Nombre, Id, etc).")
+Sintomas=st.multiselect("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",["Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"])
