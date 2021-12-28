c1014bc451bd3381b1f635627933a4289f3d5fd6
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-12-27 19:48:10 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -8,6 +8,13 @@ st.title("Bienvenido a la InfVagAPP")
 
 st.subheader("Esta es una aplicación de autocuestionamiento para orientación médica, no se registrará ningún dato que se ingresé y no se pedirá la identificación del usuario de ninguna manera (Nombre, Id, etc).")
 dolorvag="Dolor vaginal"
-Sintomas=st.multiselect("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",[dolorvag,"Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"])
+Sintomas=st.selectbox("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",[dolorvag,"Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"],0,str,None,str,callo)
 if Sintomas == dolorvag:
     st.markdown("Duele")
+
+
+#chart_data = pd.DataFrame(
+ #    np.random.randn(20, 3),
+  #   columns=['a', 'b', 'c'])
+
+#st.area_chart(chart_data)
