a221fe5b80ffb106cf172447c1097c2e60c2a112
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-01-02 06:15:12 -0600

    Infec vag app

--- APP.py
+++ APP.py
@@ -1,20 +1,23 @@
+from os import write
+from typing import Text
+from altair.vegalite.v4.schema.channels import X
+from numpy import save, sin
 import streamlit as st
 from streamlit.elements.balloons import BalloonsMixin
 from streamlit.proto.Balloons_pb2 import Balloons
 from streamlit.proto.Button_pb2 import Button
+import flask as flask
 videomp=st.image("medpost.gif")
 st.title("Bienvenido a la InfVagAPP")
 #APP para detectar infecciones vaginales y dar recomendaciones
-
-st.subheader("Esta es una aplicación de autocuestionamiento para orientación médica, no se registrará ningún dato que se ingresé y no se pedirá la identificación del usuario de ninguna manera (Nombre, Id, etc).")
-dolorvag="Dolor vaginal"
-Sintomas=st.selectbox("Selecciona de los siguientes opciones la que se aproxime más a tus síntomas",[dolorvag,"Dolor con las relaciones sexuales","Salida de líquido vaginal","Mal olor vaginal","Comezón en región vaginal"],0,str,None,str,callo)
-if Sintomas == dolorvag:
-    st.markdown("Duele")
-
-
-#chart_data = pd.DataFrame(
- #    np.random.randn(20, 3),
-  #   columns=['a', 'b', 'c'])
-
-#st.area_chart(chart_data)
+st.cache()
+col1,col2=st.beta_columns(2)
+#Parece que en multiselect tienes que poner el orden así como esta abajo supongo que como es una lista tienes que manejarlo así como tupple :X 
+with col1:
+  sintomas=st.multiselect("Síntomas",("Dolor vaginal","Comezón vaginal","Dolor con las relaciones sexuales","Flujo vaginal anormal"))
+  if "Dolor vaginal" in sintomas:
+    st.image("medpost.gif")
+  if "Comezón vaginal" in sintomas:
+    st.write("Rascaste")
+if "Dolor vaginal" and "Comezón vaginal" in sintomas:
+    st.multiselect("Alguna enfermedad que padezcas",("Diabetes mellitus","Estoy embarazada"))
